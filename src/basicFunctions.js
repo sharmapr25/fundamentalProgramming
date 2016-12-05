4a550bbaa30b796a14550ec853bea3edc87f20f8
piolAtif <spreeti@thoughtworks.com>
2016-12-05 13:30:54 +0530

    Preeti +Added isEven and isOdd tests as subset of basic function and added square test
    What changes have you made?
    
    * Added basic function test
    * Made isEven and isOdd as subsets of basic function test
    * Abstract isEven and isOdd functions in tests
    * Added a test for square
    * Added square method in basic function
    
    Why do we need this change?
    
    *
    
    What can this change impact?
    
    *

--- src/basicFunctions.js
+++ src/basicFunctions.js
@@ -4,4 +4,14 @@ basicFunctions.isEven = function(number){
 	return number%2==0;
 };
 
+basicFunctions.isOdd = function(number){
+	if(typeof(number) != 'number' || number == Infinity)
+		return false;
+	return !basicFunctions.isEven(number);
+}
+
+basicFunctions.squareOf = function(number){
+	return number*number;
+}
+
 module.exports = basicFunctions;
--- test/basicFunctionsTest.js
+++ test/basicFunctionsTest.js
@@ -1,39 +1,81 @@
 var assert = require('assert');
-var isEven = require('../src/basicFunctions.js').isEven;
+var basicFunctions = require('../src/basicFunctions.js');
 
-describe('isEven',function(){
-	it('should return true for an even number',function(){
-		assert.ok(isEven(2));
+describe('basic functions ',function(){
+	describe('isEven',function(){
+		var isEven = basicFunctions.isEven;
+		it('should return true for an even number',function(){
+			assert.ok(isEven(2));
+		});
+
+		it('should return false for an odd number',function(){
+			assert.ok(!isEven(1));
+		});
+
+		it('should return true for zero',function(){
+			assert.ok(isEven(0));
+		});
+
+		it('should return true for a negative even number',function(){
+			assert.ok(isEven(-2));
+		});
+
+		it('should return false for a negative odd number',function(){
+			assert.ok(!isEven(-1));
+		});
+
+		it('should return false if given number is an undefined',function(){
+			assert.ok(!isEven(undefined));
+		});
+
+		it('should return false if given number is an Infinity',function(){
+			assert.ok(!isEven(Infinity));
+		});
+
+		it('should return false if given element is not number type',function(){
+			assert.ok(!isEven('s'));
+		});
 	});
 
-	it('should return false for an odd number',function(){
-		assert.ok(!isEven(1));
+	describe('isOdd', function() {
+		var isOdd = basicFunctions.isOdd;
+	    it('should return true for an odd number', function() {
+	        assert.ok(isOdd(1));
+	    });
+
+	    it('should return false for an even number', function() {
+	        assert.ok(!isOdd(2));
+	    });
+
+	    it('should return false for zero', function() {
+	        assert.ok(!isOdd(0));
+	    });
+
+	    it('should return false for a negative even number', function() {
+	        assert.ok(!isOdd(-2));
+	    });
+
+	    it('should return true for a negative odd number', function() {
+	        assert.ok(isOdd(-1));
+	    });
+
+	    it('should return false if given number is an undefined', function() {
+	        assert.ok(!isOdd(undefined));
+	    });
+
+	    it('should return false if given number is an Infinity', function() {
+	        assert.ok(!isOdd(Infinity));
+	    });
+
+	    it('should return false if given element is not number type', function() {
+	        assert.ok(!isOdd('s'));
+	    });
 	});
 
-	it('should return true for zero',function(){
-		assert.ok(isEven(0));
+	describe('square',function(){
+		var squareOf = basicFunctions.squareOf;
+		it('should return 1 for given number 1',function(){
+			assert.equal(1, squareOf(1));
+		});
 	});
-
-	it('should return true for a minus even number',function(){
-		assert.ok(isEven(-2));
-	});
-
-	it('should return false for a minus odd number',function(){
-		assert.ok(!isEven(-1));
-	});
-
-	it('should return false if given number is an undefined',function(){
-		assert.ok(!isEven(undefined));
-	});
-
-	it('should return false if given number is an Infinity',function(){
-		assert.ok(!isEven(Infinity));
-	});
-
-	it('should return false if given element is not number type',function(){
-		assert.ok(!isEven('s'));
-	});
-
-	
-
 });
