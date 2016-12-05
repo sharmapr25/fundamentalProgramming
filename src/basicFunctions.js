2c2da6006ef8d8ce049cb68d54c52f688f0c17f7
piolAtif <spreeti@thoughtworks.com>
2016-12-05 17:31:59 +0530

    Preeti +Added LCM and GCD methods and tests
    What changes have you made?
    
    * Added GCD method
    * Added LCM method
    * Added tests for GCD method
    * Added tests for LCM method
    
    Why do we need this change?
    
    *
    
    What can this change impact?
    
    *

--- src/basicFunctions.js
+++ src/basicFunctions.js
@@ -8,10 +8,49 @@ basicFunctions.isOdd = function(number){
 	if(typeof(number) != 'number' || number == Infinity)
 		return false;
 	return !basicFunctions.isEven(number);
-}
+};
 
 basicFunctions.squareOf = function(number){
 	return number*number;
+};
+
+basicFunctions.cubeOf = function(number){
+	return number*basicFunctions.squareOf(number);
+};
+
+var biggerNumber = function(firstNumber,secondNumber){
+	if(firstNumber>secondNumber)
+		return firstNumber;
+	return secondNumber;
+};
+
+var smallerNumber = function(firstNumber, secondNumber){
+	if(firstNumber<secondNumber)
+		return firstNumber;
+	return secondNumber;
+}
+
+basicFunctions.gcdOf = function(firstNumber, secondNumber){
+	if(typeof(firstNumber) == 'number' && typeof(secondNumber) == 'number'){
+		var bigNumber = Math.abs(biggerNumber(firstNumber, secondNumber));
+		var smallNumber = Math.abs(smallerNumber(firstNumber, secondNumber));
+		var remainderOfdividing = bigNumber%smallNumber;
+		if(remainderOfdividing == 0)
+			return smallNumber;
+		return basicFunctions.gcdOf(remainderOfdividing, smallNumber);
+	}
+	return NaN;
+};
+
+var quotientOf = function(dividend, divisor){
+	return dividend/divisor;
+}
+
+basicFunctions.lcmOf = function(firstNumber, secondNumber){
+	gcmOfTwoNumbers = basicFunctions.gcdOf(firstNumber,secondNumber);
+	var firstQuotient = quotientOf(firstNumber, gcmOfTwoNumbers);
+	var secondQuotient = quotientOf(secondNumber, gcmOfTwoNumbers);	
+	return gcmOfTwoNumbers*firstQuotient*secondQuotient;
 }
 
 module.exports = basicFunctions;
--- test/basicFunctionsTest.js
+++ test/basicFunctionsTest.js
@@ -33,7 +33,7 @@ describe('basic functions ',function(){
 		});
 
 		it('should return false if given element is not number type',function(){
-			assert.ok(!isEven('s'));
+			assert.ok(!isEven('1'));
 		});
 	});
 
@@ -90,10 +90,6 @@ describe('basic functions ',function(){
 			assert.equal(1, squareOf(-1));
 		});
 
-		it('should return NaN when given number is undefined',function(){
-			assert.ok(isNaN(squareOf(undefined)));
-		});
-
 		it('should return Infinity when given number is Infinity',function(){
 			assert.ok(Infinity, squareOf(Infinity));
 		});
@@ -110,7 +106,124 @@ describe('basic functions ',function(){
 			assert.equal(1.0, squareOf(1.0));
 		});
 		it('should reutrn NaN when the type of given value is not a number',function(){
+			assert.ok(isNaN(squareOf(undefined)));
 			assert.ok(isNaN(squareOf('s')));
+		});
+	});
+
+	describe('cube',function(){
+		var cubeOf = basicFunctions.cubeOf;
+
+		it('should return 1 for the given value 1',function(){
+			assert.equal(1, cubeOf(1));
+		});
+
+		it('should return 8 for the given value 2',function(){
+			assert.equal(8, cubeOf(2));
+		});
+
+		it('should return -1 for the given value -1',function(){
+			assert.equal(-1, cubeOf(-1));
+		});
+
+		it('should return NaN when the given value is not a number',function(){
+			assert.ok(isNaN(cubeOf(undefined)));
+			assert.ok(isNaN(cubeOf('s')));
+		});
+
+		it('should return Infinity when the given value is Infinity',function(){
+			assert.equal(Infinity, cubeOf(Infinity));
+		});
+
+		it('should return 1.0 for the given value 1.0',function(){
+			assert.equal(1.0, cubeOf(1.0));
+		});
+
+		it('should return 0.027 for the given value 0.3',function(){
+			assert.equal(0.027, cubeOf(0.3));
+		});
+
+		it('should return -1.0 for the given value -1.0',function(){
+			assert.equal(-1.0, cubeOf(-1.0));
+		});
+
+		it('should return -Infinity when the given value is -Infinity',function(){
+			assert.equal(-Infinity, cubeOf(-Infinity));
+		});
+	});
+
+	describe('GCD',function(){
+		var gcdOf = basicFunctions.gcdOf;
+		it('should return 1 when the given values are 1 and 2',function(){
+			assert.equal(1, gcdOf(1,2));
+		});
+
+		it('should return 2 when the given values are 2 and 4',function(){
+			assert.equal(2, gcdOf(4,2));
+		});
+
+		it('should return 2 when the given values are 4 and 14',function(){
+			assert.equal(2, gcdOf(4,14));
+		});
+
+		it('should return 1 when the given values are 2 and 3',function(){
+			assert.equal(1, gcdOf(2,3));
+		});
+
+		it('should return 13 when the given values are 13 and 26',function(){
+			assert.equal(13, gcdOf(13,26));
+		});
+
+		it('should return 2 when the given values are 18 and 16',function(){
+			assert.equal(2,gcdOf(18,16));
+		});
+
+		it('should return 2 when the given values are -8 and -10',function(){
+			assert.equal(2, gcdOf(-8,-10));
+		});
+
+		it('should return NaN when the given values are not a number',function(){
+			assert.ok(isNaN(gcdOf(2, undefined)));
+			assert.ok(isNaN(gcdOf('s',2)));
+			assert.ok(isNaN(gcdOf(undefined, undefined)));
+		});
+	});
+
+	describe('LCM',function(){
+		var lcmOf = basicFunctions.lcmOf;
+
+		it('should return 2 when the given values are 1 and 2',function(){
+			assert.equal(2, lcmOf(1,2));
+		});
+
+		it('should return 4 when the given values are 2 and 4',function(){
+			assert.equal(4, lcmOf(2,4));
+		});
+
+		it('should return 2 when the given values are 4 and 14',function(){
+			assert.equal(28, lcmOf(4,14));
+		});
+
+		it('should return 1 when the given values are 2 and 3',function(){
+			assert.equal(6, lcmOf(2,3));
+		});
+
+		it('should return 13 when the given values are 13 and 26',function(){
+			assert.equal(26, lcmOf(13,26));
+		});
+
+		it('should return 2 when the given values are 18 and 16',function(){
+			assert.equal(144,lcmOf(18,16));
+		});
+
+		it('should return 2 when the given values are -8 and -10',function(){
+			assert.equal(40, lcmOf(-8,-10));
+		});
+
+		it('should return NaN when the given values are not a number',function(){
+			assert.ok(isNaN(lcmOf(2, undefined)));
+			assert.ok(isNaN(lcmOf('s',2)));
+			assert.ok(isNaN(lcmOf(undefined, undefined)));
 		});
 	});
 });
