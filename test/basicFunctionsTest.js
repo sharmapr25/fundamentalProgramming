38c2c518c2b9b1c61d7046afaefa9806a8b0afc2
piolAtif <spreeti@thoughtworks.com>
2016-12-05 14:51:49 +0530

    Added tests for square

--- test/basicFunctionsTest.js
+++ test/basicFunctionsTest.js
@@ -74,8 +74,40 @@ describe('basic functions ',function(){
 
 	describe('square',function(){
 		var squareOf = basicFunctions.squareOf;
-		it('should return 1 for given number 1',function(){
+		it('square of 1 should be 1',function(){
 			assert.equal(1, squareOf(1));
+		});
+
+		it('square of zero should be 0',function(){
+			assert.equal(0, squareOf(0));
+		});
+
+		it('square of ten should be 100',function(){
+			assert.equal(100, squareOf(10));
+		});
+
+		it('square of negative 1 should be 1',function(){
+			assert.equal(1, squareOf(-1));
+		});
+
+		it('should return NaN when given number is undefined',function(){
+			assert.ok(isNaN(squareOf(undefined)));
+		});
+
+		it('should return Infinity when given number is Infinity',function(){
+			assert.ok(Infinity, squareOf(Infinity));
+		});
+
+		it('should return Infinity when given number is negative Infinity',function(){
+			assert.ok(Infinity, squareOf(Infinity));
+		});
+
+		it('square of 0.6 should be 3.6',function(){
+			assert.equal(0.36, squareOf(0.6));
+		});
+
+		it('squareOf 1.0 should be 1.0',function(){
+			assert.equal(1.0, squareOf(1.0));
 		});
 	});
 });
