eaf1d5368ad0a98a15f0bc88b49fd1a8d07312ae
piolAtif <spreeti@thoughtworks.com>
2016-12-05 13:07:12 +0530

    preeti  +Added tests for isEven function
    What changes have you made?
    
    *
    
    Why do we need this change?
    
    *
    
    What can this change impact?
    
    *

--- src/basicFunctions.js
+++ src/basicFunctions.js
@@ -1,7 +1,7 @@
 var basicFunctions = {};
 
 basicFunctions.isEven = function(number){
-	return number;
+	return number%2==0;
 };
 
 module.exports = basicFunctions;
--- test/basicFunctionsTest.js
+++ test/basicFunctionsTest.js
@@ -5,4 +5,35 @@ describe('isEven',function(){
 	it('should return true for an even number',function(){
 		assert.ok(isEven(2));
 	});
+
+	it('should return false for an odd number',function(){
+		assert.ok(!isEven(1));
+	});
+
+	it('should return true for zero',function(){
+		assert.ok(isEven(0));
+	});
+
+	it('should return true for a minus even number',function(){
+		assert.ok(isEven(-2));
+	});
+
+	it('should return false for a minus odd number',function(){
+		assert.ok(!isEven(-1));
+	});
+
+	it('should return false if given number is an undefined',function(){
+		assert.ok(!isEven(undefined));
+	});
+
+	it('should return false if given number is an Infinity',function(){
+		assert.ok(!isEven(Infinity));
+	});
+
+	it('should return false if given element is not number type',function(){
+		assert.ok(!isEven('s'));
+	});
+
+	
+
 });
