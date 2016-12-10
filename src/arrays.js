c4373de5459ee816203b6ecac40b5e064ad5a347
piolAtif <spreeti@thoughtworks.com>
2016-12-10 10:48:19 +0530

    Added tests for Arrays

---
+++ src/arrays.js
@@ -0,0 +1,51 @@
+var arrays = {};
+var basic = require('./basicFunctions.js');
+
+arrays.oddNumbers = function(list){
+	return list.filter(basic.isOdd);
+};
+
+arrays.evenNumbers = function(list){
+	return list.filter(basic.isEven);
+};
+
+var sumOf = function(first, second){
+	return first+second;
+};
+
+arrays.sumOfNumbers = function(list){
+	return list.reduce(sumOf);
+};
+
+var reverseOrder = function(initial, current, index, list){
+	initial.unshift(current);
+	return initial;
+};
+
+arrays.printReverseOrder = function(list){
+	newList = list.reduce(reverseOrder,[]);
+	return newList.join("\n");
+};
+
+arrays.eachSecondElement = function(list){
+	return list.filter(function(number, index){
+		return basic.isEven(index);
+	});
+};
+
+var fibo = function(number){
+	if(number == 0 || number == 1)
+		return number;
+	return fibo(number-1)+fibo(number-2);
+};
+
+arrays.fiboReverse = function(number){
+	var list = Array.apply(null, Array(number));
+	list.forEach(function(d, i, l){
+		l.pop(l[i]);
+		l.unshift(fibo(i))
+	});
+	return list;
+};
+
+module.exports = arrays;
---
+++ test/ArraysTest.js
@@ -0,0 +1,35 @@
+var assert = require('assert');
+var arrays = require('../src/arrays');
+
+
+describe('array', function(){
+	describe('for single array',function(){
+		var list = [1,2,3,4];
+		
+		it('should return odd numbers',function(){
+			assert.deepEqual([1,3], arrays.oddNumbers(list));
+		});
+
+		it('should return even numbers',function(){
+			assert.deepEqual([2,4], arrays.evenNumbers(list));
+		});
+
+		it('should return sum of numbers',function(){
+			assert.deepEqual(10, arrays.sumOfNumbers(list));
+		});
+
+		it('should return list in reverse order',function(){
+			var expected = "4\n3\n2\n1";
+			assert.equal(expected, arrays.printReverseOrder(list));
+		});
+
+		it('should return every second element',function(){
+			assert.deepEqual([1,3],arrays.eachSecondElement(list));
+		});
+
+		it('should return reverse fibonacci for given number',function(){
+			var expected = [13,8,5,3,2,1,1,0];
+			assert.deepEqual(expected, arrays.fiboReverse(8));
+		});
+	});
+});
